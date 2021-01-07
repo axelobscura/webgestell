4a59fe25da549c18297777cfcb6773d9ade09e10
Axel <axosar@gmail.com>
2021-01-06 20:04:58 -0600

    Contacto

---
+++ pages/contacto.js
@@ -0,0 +1,45 @@
+import React from "react";
+import Layout from './components/Layout';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+export default function Contacto() {
+  return (
+    <Layout>
+      <div className="component second-component">
+        <h2 className="titularG">Comunícate con nosotros</h2>
+        <hr />
+        <Form className="contacto">
+          <Form.Group controlId="formBasicEmail">
+            <Form.Control type="email" placeholder="NOMBRE" />
+          </Form.Group>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Control type="email" placeholder="APELLIDOS" />
+          </Form.Group>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Control type="email" placeholder="EMAIL" />
+          </Form.Group>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Control type="email" placeholder="EMPRESA" />
+          </Form.Group>
+          <Button variant="primary" type="submit" style={{ background: '#000', border: 'none'}}>
+            Submit
+          </Button>
+        </Form>
+      </div>
+      <style jsx>{`
+        .second-component {
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          align-content: center;
+          height: 100vh;
+        }
+        .second-component form.contacto {
+          width: 90%;
+        }
+      `}</style>
+    </Layout>
+  )
+}
